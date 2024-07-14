288b765ab5a89b8d0ebcfd84766b6dca059c922a
LuisHugoMH <121759262+LuisHugoMH@users.noreply.github.com>
2024-07-13 21:05:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-// script.js
-let model;
-
 async function loadModel() {
     try {
-        // Actualiza esta URL con la ruta correcta de tu repositorio de GitHub Pages
-        model = await tf.loadLayersModel('https://luishugomh.github.io/Maiz_1/model/model.json');
-        console.log('Modelo cargado correctamente');
+        const model = await tf.loadLayersModel('model/model.json');
+        return model;
     } catch (error) {
         console.error('Error al cargar el modelo:', error);
     }
 }
 
-async function predictImage(image) {
-    const img = tf.browser.fromPixels(image)
-        .resizeNearestNeighbor([256, 256])
-        .toFloat()
-        .expandDims(0);
-
-    const prediction = await model.predict(img).data();
-    return prediction;
-}
-
 document.getElementById('imageInput').addEventListener('change', async (event) => {
     const file = event.target.files[0];
-    const imageElement = document.getElementById('selectedImage');
     const reader = new FileReader();
 
-    reader.onload = async function(e) {
-        imageElement.src = e.target.result;
-        imageElement.onload = async function() {
-            const prediction = await predictImage(imageElement);
-            document.getElementById('prediction').innerText = `Predicción: ${prediction}`;
-        }
+    reader.onload = async function () {
+        const img = new Image();
+        img.src = reader.result;
+        img.onload = async function () {
+            const tensor = tf.browser.fromPixels(img).resizeNearestNeighbor([256, 256]).toFloat().expandDims();
+            const model = await loadModel();
+            if (model) {
+                const predictions = model.predict(tensor).dataSync();
+                const predictedClass = tf.argMax(predictions).dataSync()[0];
+                const classes = ['Enfermo', 'Sano']; // Ajusta esto según tus clases
+                document.getElementById('prediction').innerText = `Maíz ${classes[predictedClass]}`;
+            } else {
+                document.getElementById('prediction').innerText = 'Error al cargar el modelo';
+            }
+        };
+        document.getElementById('selectedImage').src = reader.result;
     };
 
-    if (file) {
-        reader.readAsDataURL(file);
-    }
+    reader.readAsDataURL(file);
 });
-
-window.addEventListener('load', loadModel);
